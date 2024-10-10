529c61937602d4ed701708310d05c1255cb3a284
João Victor Colussi <victorcolussi@outlook.com>
2024-10-10 18:19:24 -0300

    Feito o segundo exercicio do curso que é comentar variáveis

---
+++ exercicio2.php
@@ -0,0 +1,7 @@
+<?php
+
+$nome_usuario = "Mario Gonzada"; // este é o nome do usuário
+$altura_pessoa = 1.70; // esta é a altura dele
+
+echo "A altura do usuário é : $nome_usuario e sua altura é $altura_pessoa"
+?>
