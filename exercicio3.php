c25098b170799ee329977c13a7f41eeddc8dc580
João Victor Colussi <victorcolussi@outlook.com>
2024-10-10 18:55:30 -0300

    Aprendendi uma função de ver se o número digitado é inteiro

--- exercicio3.php
+++ exercicio3.php
@@ -7,12 +7,16 @@ $vezescinco = $numero * 5;
 printf("O número que digitou x5 é: $vezescinco\n");
 
 if ($vezescinco > 50){
-  printf("O número é maior que 50");
+  printf("O número é maior que 50\n");
 }else{
-  printf("O número é menor que 50");
+  printf("O número é menor que 50\n");
 }
 
 
-
+if(is_int($numero)){
+  printf("\nO número $numero é inteiro");
+}else{
+  printf("O número não é inteiro");
+}
 
 ?>
