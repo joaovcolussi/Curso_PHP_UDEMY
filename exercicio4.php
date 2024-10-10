16179528afb77bed9097f15f645bfa5df1a790bc
João Victor Colussi <victorcolussi@outlook.com>
2024-10-10 19:09:41 -0300

    Fiz o exercicio 4 do curso de php

---
+++ exercicio4.php
@@ -0,0 +1,24 @@
+<?php
+$n1 = 4.3;
+$n2 = 4;
+$n3 = "Amora";
+
+
+if (is_float($n1)){
+  echo "O tipo do valor do $n1 é float \n";
+}else{
+  echo "O valor do $n1 não é do tipo float\n";
+}
+
+if (is_float($n2)){
+  echo "O tipo do valor do $n2 é float \n";
+}else{
+  echo "O valor do $n2 não é do tipo float\n";
+}
+if (is_float($n3)){
+  echo "O tipo do valor do $n3 é float \n";
+}else{
+  echo "O valor do $n3 não é do tipo float\n";
+}
+
+?>
