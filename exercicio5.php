385fdd4422b5dba7cb12f2cfe53de0ce022ea794
João Victor Colussi <victorcolussi@outlook.com>
2024-10-11 10:46:45 -0300

    Testando se o valor é do tipo booleano do exercicio 5

--- exercicio5.php
+++ exercicio5.php
@@ -3,10 +3,26 @@ $str = "Eu amo carne";
 
 
 if (is_string($str)) {
-  echo "$str é uma string";
+  echo "$str é uma string\n";
 }else{
-  echo "$str Não é uma string";
+  echo "$str Não é uma string\n";
 }
 
+$str = true;
+
+if (is_bool($str)){
+  echo "O $str é true\n";
+}else{
+  echo "O $str é false\n";
+}
+
+$str = true;
+
+
+if (is_bool($str == false)){
+  echo "O $str é true";
+}else{
+  echo "O $str é false";
+}
 
 ?>
