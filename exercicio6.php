e608b8d2b8219b3d639fb3a5fa64c4706bf2c693
João Victor Colussi <victorcolussi@outlook.com>
2024-10-11 11:04:55 -0300

    Feito o exercicio6 do curso de php com arrays associativas

---
+++ exercicio6.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$arr_normal = ["Batatas", 1 , true, "Catapimbas"];
+$arr_assosciativo = ["Marca" => "Toyota", "Cor" => "Branco Perolizado", "Rodas" => 4];
+
+print_r($arr_assosciativo);
+
+print_r($arr_assosciativo['Marca']);
+
+print_r($arr_normal);
+
+$marca= $carro['Marca'];
+
+?>
