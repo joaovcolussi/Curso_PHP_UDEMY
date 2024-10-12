cc85373d7d08db85010b5f04feb535574d1065fd
João Victor Colussi <victorcolussi@outlook.com>
2024-10-12 12:32:16 -0300

    Melhorando o exercicio 8

--- exercicio8.php
+++ exercicio8.php
@@ -8,8 +8,11 @@ $variavel_true= True;
 $itens = ["Teto Solar", "Motor 2.0", "Porta Malas 40L","Painel Digital"];
 
 print($variavel_nome);
+echo "/n";
 print($variavel_numero);
+echo "/n";
 print($variavel_true) ;
+echo "/n";
 print_r($itens);
 
 
