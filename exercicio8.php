9602d2b2efbcaa339258a3764296ff04ce1c3136
João Victor Colussi <victorcolussi@outlook.com>
2024-10-12 12:30:26 -0300

    Adicionei um array no exercicio8

--- exercicio8.php
+++ exercicio8.php
@@ -5,9 +5,12 @@ $variavel_numero = 32312;
 
 $variavel_true= True;
 
-echo $variavel_nome;
-echo $variavel_numero;
-echo $variavel_true;
+$itens = ["Teto Solar", "Motor 2.0", "Porta Malas 40L","Painel Digital"];
+
+print($variavel_nome);
+print($variavel_numero);
+print($variavel_true) ;
+print_r($itens);
 
 
 ?>
