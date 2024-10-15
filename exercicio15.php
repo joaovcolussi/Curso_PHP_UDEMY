8daec41f84738ed438447bc922accec8b835c2db
João Victor Colussi <victorcolussi@outlook.com>
2024-10-15 09:39:46 -0300

    Feito o exercicio15 do curso onde mostra se o valor é igual ou diferente com operadores de comparação

---
+++ exercicio15.php
@@ -0,0 +1,13 @@
+<?php
+function igualoudiferente($a,$b){
+  if ($a == $b){
+    echo "Os números são iguais";
+  }else{
+    echo "Os números são diferentes";
+}
+}
+
+echo igualoudiferente(4,4);
+
+
+?>
