8a3e279f861aeda3cc620161bdfa8ec61f4b2ea9
João Victor Colussi <victorcolussi@outlook.com>
2024-10-16 10:41:56 -0300

    Feito o exercicio20 do curso de php onde uso operador ternario e operador de atribuição

---
+++ exercicio20.php
@@ -0,0 +1,25 @@
+<?php
+$a = 0;
+$b = 0;
+
+$a +=20;
+echo $a;
+echo "\n";
+$a -=10;
+echo $a;
+echo "\n";
+$a /=5;
+echo $a;
+$a +=20;
+echo "\n";
+echo $a;
+echo "\n";
+$a %=2;
+echo $a;
+echo "\n";
+
+echo 5>2 ? "Verdadeiro" : "Falso";
+echo "\n";
+echo 3>5 ? "Verdadeiro" : "Falso";
+
+?>
