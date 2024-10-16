34d301e2c4c28d47e1ee5421ae7033890a503bbe
João Victor Colussi <victorcolussi@outlook.com>
2024-10-16 09:38:26 -0300

    Feito exercicio17 do curso de php onde se utiliza o AND

---
+++ exercicio17.php
@@ -0,0 +1,16 @@
+<?php
+function checagem($a,$b){
+  $a = NULL;
+  $b = NULL;
+
+  if ($b > 5 AND "João" ===$a){
+    echo "Os valores são iguais!";
+  }else{
+    echo "Os valores não são iguais";
+  }
+}
+
+echo checagem("João",6);
+
+
+?>
