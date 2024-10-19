c35480e6422d843e86126f9dacded1358632fb33
João Victor Colussi <victorcolussi@outlook.com>
2024-10-19 16:40:17 -0300

    Feito o exercicio21 do curso de PHP da udemy

---
+++ exercicio21.php
@@ -0,0 +1,27 @@
+<?php
+ echo 5 > 2 ?  "Sim o valor é maior\n" : "Não é maior\n";
+
+
+ $n1 = 5;
+ $n2 = 2;
+
+ if ($n1 > $n2){
+  echo "O primeiro valor $n1 é maior que o segundo \n";
+ }else{
+  echo "O segundo valor $n2 é maior \n";
+ }
+
+ if ("Matheus" != "Pedro"){
+  echo "Sim, Matheus é diferente de pedro\n";
+ }else{
+  echo "Não, Matheus é igual a pedro\n";
+ }
+
+ $n1 = 12;
+ $n2 = 11;
+ if ($n1 <= $n2){
+  echo "O valor $n1 é menor ou igual a 11\n";
+  }else{
+    echo "O valor $n2 é maior que 11\n";
+  }
+?>
