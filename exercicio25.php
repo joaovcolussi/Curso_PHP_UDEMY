a1a2ed76ab915eb147798c9395cc477807a2e09c
João Victor Colussi <victorcolussi@outlook.com>
2024-10-22 09:29:11 -0300

    Feito o exercicio25 do curso de php onde coloca um if dentro do outro

---
+++ exercicio25.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$varnumero1 = 50;
+$varnumero2 = 0;
+$varstring = "Macarrão";
+
+if (is_int($varnumero1)){
+  echo "O primeiro if é um número";
+  if (is_int($varnumero1)){
+    $varnumero1*= 2 + $varnumero2;
+    echo $varnumero2;
+    if ($varnumero2 >100){
+      echo "$varnumero2 é maior que 100";
+    }else{
+      echo "O terceiro if não é verdadeiro";
+    }
+  }else
+  echo "O segundo if não é um número"; 
+}else{
+  echo "O primeiro if não é um número";
+}
+
+
+
+?>
