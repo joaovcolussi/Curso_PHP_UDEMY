1145bf73a7df832201e37c27a87e8b3f390982d8
João Victor Colussi <victorcolussi@outlook.com>
2024-12-06 19:45:03 -0300

    Feito o exercicio37 do curso de php

---
+++ exercicio37.php
@@ -0,0 +1,7 @@
+<?php
+function defineCorCarro($corAlterada,$corPadrao= "Vermelha"){
+ echo "A cor padrão do carro é $corPadrao e a cor que foi alterada é $corAlterada" . "\n";
+}
+
+defineCorCarro("Azul");
+?>
---
+++ teste.php
@@ -0,0 +1,17 @@
+<?php
+
+function teste($a = "teste"){
+
+  echo "O valor de A é $a " . "\n";
+}
+
+teste();
+
+
+
+function testando ($b,$a = "x"){
+  echo "O valor de a é $a e de b é : $b" . "\n";
+}
+
+
+testando(1);
